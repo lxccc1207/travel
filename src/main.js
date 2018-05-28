660dfe031a86711b70a1ea0599f6210b3dcdf562
lxccc1207 <414844730@qq.com>
2018-05-28 11:03:51 +0800

    '增加轮播'

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import router from './router'
 import '@@/reset.css'
 import '@@/border.css'
 import '@@/iconfont.css'
-Vue.config.productionTip = false
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
+Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList">
+      <img class="swiper-img" :src="item.imgUrl" :key="item.id" />
+    </swiper-slide>
+
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+    <!--<div class="swiper-button-next" slot="button-next"></div>-->
+    <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HomeSwiper',
+  data(){
+      return{
+        swiperOption:{
+          pagination:'.swiper-pagination', //用于分页的点
+          loop:true
+        },
+        swiperList:[
+          {
+            id:'0001',
+            imgUrl:'https://imgs.qunarzz.com/piao/fusion/1804/bd/8e4a1c3f470d3702.jpg_640x170_c1e8baeb.jpg'
+          },
+          {
+            id:'0002',
+            imgUrl:'https://imgs.qunarzz.com/piao/fusion/1710/94/e41712475cc4e702.jpg_640x170_f5dd3e83.jpg'
+          },
+          {
+            id:'0003',
+            imgUrl:'https://imgs.qunarzz.com/piao/fusion/1804/5c/969bfa7af00a2702.jpg_640x170_ec465291.jpg'
+          },
+
+        ]
+      }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper>>> .swiper-pagination-bullet-active/*三个箭头表示样式穿透，不受scoped的影响*/
+   background:#fff !important
+  .wrapper
+    width:100%
+    /*height:26.56vw 兼容性不好，考虑传统写法*/
+    overflow:hidden
+    height:0
+    padding-bottom:26.56%
+    background:#eee
+
+    .swiper-img
+      width:100%
+</style>
