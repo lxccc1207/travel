6998cd1189323360eab9731447b482b40e12e9fc
lxccc1207 <414844730@qq.com>
2018-06-01 11:13:56 +0800

    景点详情渐隐渐现效果

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <detail-banner></detail-banner> 
+    <detail-header></detail-header> 
+    <div class="content"></div>   <!--帮助撑开页面高度-->
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default{
   name:'Detail',
   components:{
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height:50rem
 </style>
